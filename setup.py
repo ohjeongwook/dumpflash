73581d9ec266e4cfec0d0b8d9ede4b4c686d10fd
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-12-10 18:40:06 -0800

    Added setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dumpflash-ohjeongwook",
+    version="0.0.1",
+    author="Matt Oh",
+    author_email="jeongoh@darungrim.com",
+    description="DumpFlash Tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ohjeongwook/dumpflash",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
