18d7d04783c66a375e46a57a5d25e96b45080a37
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-12-10 18:47:54 -0800

    Added pyftdi dependencies

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ohjeongwook/dumpflash",
     packages=setuptools.find_packages(),
+    install_requires=[
+          'pyftdi',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
